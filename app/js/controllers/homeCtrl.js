abb80ee7c066fe3a7971d86e39718ad54a6dd2b6
Rahul Singh <rsingh@infinitysoftsystems.com>
2023-09-13 13:30:52 +0530

    home page issue fixed

--- app/js/controllers/homeCtrl.js
+++ app/js/controllers/homeCtrl.js
@@ -594,28 +594,26 @@ angular
               //console.log(markerShape, 'markerShape red else')
             } else {
 
-              if (aLocation[i].alertOne != 'undefined' && aLocation[i].distanceValue != "" && aLocation[i].distanceValue < parseInt(aLocation[i].alertOne)) {
+              if (aLocation[i].alertOne != undefined && aLocation[i].distanceValue != "" && aLocation[i].distanceValue < parseInt(aLocation[i].alertOne)) {
                 var markerShape = "circle";
                 //aLocation[i].markerOnMap.push(markerShape);
                 aLocation[i].markerOnMap = markerShape;
                 console.log(markerShape, 'markerShape circle')
-                 if (aLocation[i].alertTwo != 'undefined' && aLocation[i].distanceValue < parseInt(aLocation[i].alertTwo)) {
+              }
+                 if (aLocation[i].alertTwo != undefined && aLocation[i].distanceValue < parseInt(aLocation[i].alertTwo)) {
                   var markerShape = "square";
                   //aLocation[i].markerOnMap.push(markerShape);
                   aLocation[i].markerOnMap = markerShape;
                   console.log(markerShape, 'markerShape square')
-                  if (aLocation[i].alertThree != 'undefined' && aLocation[i].distanceValue != "" && aLocation[i].distanceValue < parseInt(aLocation[i].alertThree)) {
+                }
+                  if (aLocation[i].alertThree != undefined && aLocation[i].distanceValue != "" && aLocation[i].distanceValue < parseInt(aLocation[i].alertThree)) {
                     var markerShape = "triangle";
                     //aLocation[i].markerOnMap.push(markerShape);
                     aLocation[i].markerOnMap = markerShape;
                     console.log(markerShape, 'markerShape triangle')
                   }
-                  
-                } 
-  
-                
-              }
-              else if (aLocation[i].alertOne == 'undefined' && aLocation[i].distanceValue != "" && aLocation[i].alertTwo == 'undefined' && aLocation[i].alertThree == 'undefined') {
+              
+              else if (aLocation[i].alertOne == undefined && aLocation[i].distanceValue != "" && aLocation[i].alertTwo == undefined && aLocation[i].alertThree == undefined) {
                 var markerShape = "green";
                 aLocation[i].markerOnMap = markerShape;
                 console.log(markerShape, 'markerShape green')
