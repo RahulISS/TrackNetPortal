ef207e675358363d0745d12c00dcecfcad4b6573
Pravin Kumar <pravin@infinitysoftsystems.com>
2023-10-04 09:56:31 +0530

    remove , colunm in csv

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,8 @@ angular.module('mobileWaterTrac', ['angularModalService','ui.router','app.routes
 'loginCtrl','mainCtrl','homeCtrl','summaryCtrl','realTimeCtrl','overviewCtrl',
 'chartsCtrl','authService','dataService','highcharts-ng','ngStorage','d3',
 'settingsCtrl','imageCtrl','tracknetCtrl']).config(function($provide) {
-    // for local
-   //   $provide.constant('apiBaseUrl', 'http://127.0.0.1:8000/api/v1/'); 
-    // for live
-   $provide.constant('apiBaseUrl', 'https://dev-api-sg.tracwater.asia/api/v1/');
+    //for local
+    // $provide.constant('apiBaseUrl', 'http://127.0.0.1:8000/api/v1/'); 
+    //for live
+    $provide.constant('apiBaseUrl', 'https://dev-api-sg.tracwater.asia/api/v1/');
 });
